1e4a3c3ff08bc61589e217130ffac268e74b3e13
Jonathan Huang <grokut@gmail.com>
2015-03-10 15:16:25 -0700

    code clean

--- app/scripts/annotate-script.js
+++ app/scripts/annotate-script.js
@@ -1,5 +1,8 @@
 
+var tabUrl = window.location.href.toString();
 var Color = 'Yellow'
+var selection = window.getSelection();
+var ranges;
 
 var Highlighter = rangy.createHighlighter();
 
@@ -10,99 +13,53 @@ var Pink = rangy.createClassApplier('Pink');
 var Aqua = rangy.createClassApplier('Aqua');
 var Yellow = rangy.createClassApplier('Yellow');
 
-Highlighter.addClassApplier(Red)
-Highlighter.addClassApplier(Blue)
-Highlighter.addClassApplier(Green)
-Highlighter.addClassApplier(Pink)
-Highlighter.addClassApplier(Aqua)
-Highlighter.addClassApplier(Yellow)
+Highlighter.addClassApplier(Red);
+Highlighter.addClassApplier(Blue);
+Highlighter.addClassApplier(Green);
+Highlighter.addClassApplier(Pink);
+Highlighter.addClassApplier(Aqua);
+Highlighter.addClassApplier(Yellow);
 
 $(function() {
-
-  var url = window.location.href.toString();
-  var ranges;
-
-  initialize();
-  
-  $('body').mouseup(function(){
-    Highlighter.highlightSelection(Color)
-    ranges = Highlighter.serialize();
-    saveHighlights(ranges);
+  reloadHighlights();
+  $('body').mouseup( function() {
+    if ( selection.type === "Range" ){ 
+      Highlighter.highlightSelection(Color);
+      ranges = Highlighter.serialize();
+      saveHighlights(ranges);
+    }
   });
 });
 
 // Message Listener
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-   console.log(request, " received from ", sender)
-
    if (request.popup) {
-    console.log("the COLOR was ", Color)
     Color = request.popup;
-    console.log("the COLOR is now ", Color)
    }
-
    if (request.remove) {
     Highlighter.removeAllHighlights();
    }
-
   }
 );
 
-function initialize(){
-
-  // Request Tab URL from Background
-  chrome.runtime.sendMessage({greeting: "What is my URL?"}, function(response) {
-   
-    var url = response.url.toString();
-
-    chrome.storage.local.get(function (result) {
-      console.log('This is the Storage',result)
-      
-      // If URL does not exist on storage, then create storage array
-      if (result[url] === undefined){
-
-        var urlStorage={} 
-            urlStorage[url]=[];
-
-        chrome.storage.local.set(urlStorage, function(result){
-          console.log("Added URL to storage")
-        })
-
-      // URL already exists in storage, then repopulate highlights
-      } else {
-        console.log("The URL exists in storage")
-        console.log("starting deserialization")
-
-        var ranges = result[url]
-
-        Highlighter.deserialize(ranges);
-      }
-    });
+function reloadHighlights(){
+  chrome.storage.local.get( function (storage) {
+    if (storage[tabUrl]){
+      ranges = storage[tabUrl];
+      Highlighter.deserialize(ranges);
+    }
   });
-
 };
 
 function saveHighlights(ranges){
-
-  var url = window.location.href.toString();
-  console.log(url)
-
-  chrome.storage.local.get(function(result){
-
-    if ( result[url] === undefined ){
-      console.log("ERROR URL DOESNT EXIST ON STORAGE");
-    } else {
-      result[url] = ranges
-    }
-
-    console.log("ADDED ",result[url])
-
-    chrome.storage.local.set(result, function(){
-      console.log('success')
+  chrome.storage.local.get( function (storage) {
+    storage[tabUrl] = ranges;
+    chrome.storage.local.set(storage, function(){
+      console.log('saved higlights');
     });
   });  
-}
+};
 
 
 
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,8 +1,6 @@
 var background = chrome.extension.getBackgroundPage();
 var activeTab;
 
-background.console.log('\'Allo \'Allo! Popup 123123');
-
 // Setting the Active Tab
 window.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({
@@ -14,24 +12,21 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-$('button#color-button').on('click', function(e){
- 
-	var color = $(this).attr('class')
-	background.console.log("you clicked the color ", color);
+$(function() {
+  
+  $('button#color-button').on('click', function(e){
+  	var color = $(this).attr('class')
+  	background.console.log("you clicked the color ", color);
+    // Send message to content script to change highlight color
+    chrome.tabs.sendMessage(activeTab, {popup: color}, function (res) {
+      console.log("Response from tab is ", res);
+    });
+  })
 
-  // Send message to content script to change highlight color
-  chrome.tabs.sendMessage(activeTab, {popup: color}, function (res) {
-    console.log(res);
-  });
-})
-
-$('button.remove').on('click', function(e){
-
-
-  background.console.log("you want to remove?");
-
-  chrome.tabs.sendMessage(activeTab, {remove: "remove all highlights"}, function (res) {
-
-    console.log(res);
-  });
-})
+  $('button.remove').on('click', function(e){
+    background.console.log("you want to remove?");
+    chrome.tabs.sendMessage(activeTab, {remove: "remove all highlights"}, function (res) {
+      console.log("Response from tab is ", res);
+    });
+  })
+});
