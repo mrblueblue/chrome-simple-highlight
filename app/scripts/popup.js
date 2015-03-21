62ced43bb4b623780967fbd544d5d0ef7d41ae52
Jonathan Huang <grokut@gmail.com>
2015-03-21 15:12:54 -0700

    corrected event listener

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,27 +1,24 @@
 var background = chrome.extension.getBackgroundPage();
 var activeTab;
 
-// Setting the Active Tab
-window.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, function(tabs) {
-    activeTab = tabs[0].id
-    background.console.log("the active tab is ", activeTab);
-  });
-});
-
 $(function() {
   
   $('button#color-button').on('click', function(e){
   	var color = $(this).attr('class')
   	background.console.log("you clicked the color ", color);
-    // Send message to content script to change highlight color
-    chrome.tabs.sendMessage(activeTab, {popup: color}, function (res) {
-      console.log("Response from tab is ", res);
+
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+      }, function(tabs) {
+        
+        activeTab = tabs[0].id
+        background.console.log("the active tab is ", activeTab);
+        chrome.tabs.sendMessage(activeTab, {popup: color}, function (res) {
+        console.log("Response from tab is ", res);
+      });
     });
-  })
+  });
 
   $('button.remove').on('click', function(e){
     background.console.log("you want to remove?");
